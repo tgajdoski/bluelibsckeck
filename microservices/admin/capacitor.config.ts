ea1ed2001e620ff8fc009fab1b08fd8dcbc9b7ea
toni gajdoski <t.gajdoski@gmail.com>
2022-11-16 16:47:54 +0100

    add capacitor

---
+++ microservices/admin/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bluelibsCap.com',
+  appName: 'bluelibsCap',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
